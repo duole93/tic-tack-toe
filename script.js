a42b3711d5ec4b48ec67e98d745a029a60759432
Duong Le <duole93@live.com>
2024-01-24 15:46:11 -0600

    create gameboard, cell and gameHandler
    Add GameBoard() function that return the board of the game.
    Add Cell() function that return a unit of the board.
    Add GameHandler() to control the game flow

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log('hello world');
+//create a new board
+function Gameboard() {
+	const board = [];
+	for (let i = 0; i < 3; i++) {
+		board[i] = [];
+		for (let j = 0; j < 3; j++) board[i][j] = Cell();
+	}
+	const getBoard = () => {
+		return board;
+	};
+
+    
+	const getConsoleBoard = () => {
+		const temp = [];
+		for (let i = 0; i < 3; i++) {
+			temp.push(
+				board[i].map((item) => {
+					return item.getValue();
+				})
+			);
+		}
+		console.log("board ",  temp);
+	};
+
+	const placeMark = (row, column, mark) => {
+        //check valid move
+		board[row][column].setMark(mark);
+	};
+
+	return { getBoard, getConsoleBoard, placeMark };
+}
+
+//cell is a unit of a board.
+function Cell() {
+	let value = 0;
+	const setMark = (mark) => {
+		value = mark;
+	};
+	const getValue = () => value;
+	return { setMark, getValue };
+}
+
+//game control
+function GameHandler(){
+    const newBoard = Gameboard();
+    const player1 = {no: 1, mark: 1}
+    const player2 = {no: 2, mark: 2}
+    let activePlayer = player1;
+    const switchPlayer = ()=>{
+        activePlayer = activePlayer === player1?player2:player1;
+    }
+
+}
